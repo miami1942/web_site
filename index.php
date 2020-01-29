375d6751a48fa4427bba5950dd2d22e96f86bdb2
JHJ <wkdgmd7386@gmail.com>
2020-01-29 23:56:30 +0900

    * 이미지 삽입, 링크걸기, 크기조절 * 구역 나누기(html5의미요소 사용), 구역 색칠하기, 구역 패딩 * 임시 리스트 작성

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>HTML 일기장</title>
+
+
+    <style>
+    header{background-color:yellow;text-align: center;}
+    section{background-color:limegreen;}
+    aside{background-color: orange;}
+    footer{background-color: pink;}
+    .main_img{padding: 20px;}
+    </style>
+
+
+  </head>
+  <body>
+    <header>
+      <!--       로고 및 제목       -->
+      <a href="http://localhost/web/index.php">
+        <img class="main_img" src="images/main_image.png"
+             width="300" height="150">
+      </a>
+    </header>
+<!--######################################################-->
+    <section>
+      <!--       목록       -->
+      <ul>
+        <li>가상의 리스트1</li>
+        <menu>리스트1 메뉴1</menu>
+        <menu>리스트1 메뉴2</menu>
+        <menu>리스트1 메뉴3</menu>
+        <li>가상의 리스트2</li>
+        <li>가상의 리스트3</li>
+      </ul>
+    </section>
+<!--######################################################-->
+    <aside>
+      <!--       본문내용      -->
+      "main view"
+    </aside>
+<!--######################################################-->
+    <footer>
+      <!--       전화번호등 참고사항      -->
+      phone 000-0000-0000
+      fax 0000-0000-0000-0000<br>
+      home aaaa-bbbb-ccc-000d-000h<br>
+    </footer>
+  </body>
+</html>
---
+++ lib/db.php
@@ -0,0 +1,10 @@
+<!--
+본문에
+require("lib/db.php");
+함수();
+추가
+-->
+function db_init(){
+  ...
+  return -1;
+}
