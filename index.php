c4d5c549af24b5d87502ce41c9857cbe1eea5f2d
JHJ <wkdgmd7386@gmail.com>
2020-02-01 17:10:49 +0900

    내용 php id로 바꾸기전 백업파일

--- index.php
+++ index.php
@@ -3,44 +3,57 @@
   <head>
     <meta charset="utf-8">
     <title>HTML 일기장</title>
-
-
-    <style>
-    header{background-color:yellow;text-align: center;}
-    section{background-color:limegreen;}
-    aside{background-color: orange;}
-    footer{background-color: pink;}
-    .main_img{padding: 20px;}
-    </style>
-
+    <link rel="stylesheet" href="css/style.css">
 
   </head>
   <body>
     <header>
       <!--       로고 및 제목       -->
-      <a href="http://localhost/web/index.php">
+      <a href="index.php">
         <img class="main_img" src="images/main_image.png"
              width="300" height="150">
       </a>
     </header>
-<!--######################################################-->
-    <section>
+    <!--######################################################-->
+    <aside>
       <!--       목록       -->
       <ul>
-        <li>가상의 리스트1</li>
-        <menu>리스트1 메뉴1</menu>
-        <menu>리스트1 메뉴2</menu>
-        <menu>리스트1 메뉴3</menu>
-        <li>가상의 리스트2</li>
-        <li>가상의 리스트3</li>
+        <li id="M_list">가상의 리스트1</li>
+        <menu id="S_list"><a href="php/000.php">리스트1 메뉴1</a></menu>
+        <menu id="S_list"><a href="php/001.php">리스트1 메뉴2</a></menu>
+        <menu id="S_list"><a href="php/002.php">리스트1 메뉴3</a></menu>
+        <li id="M_list">가상의 리스트2</li>
+        <li id="M_list">가상의 리스트3</li>
       </ul>
-    </section>
-<!--######################################################-->
-    <aside>
-      <!--       본문내용      -->
-      "main view"
     </aside>
 <!--######################################################-->
+    <section id="main_side">
+      <article>
+        <!--       본문내용      -->
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.<br>
+        이곳은 메인 페이지 입니다.
+      </article>
+    </section>
+  <!--######################################################-->
     <footer>
       <!--       전화번호등 참고사항      -->
       phone 000-0000-0000
